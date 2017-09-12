6adcbe37c0e1229f9315542c856d22e9bf80b12a
Samuel SIlva <samuelsilvawb@gmail.com>
2017-09-12 23:28:34 +0000

    Create deepin.sh

---
+++ deepin.sh
@@ -0,0 +1,24 @@
+
+sudo apt-get update;
+
+sudo apt-get install openjdk-8-jdk;
+
+sudo apt-get install git;
+
+sudo apt-get install maven;
+
+sudo apt-get install build-essential;
+
+sudo apt-get install ruby;
+
+sudo gem install foundation;
+
+sudo gem install compass;
+
+sudo gem install sass;
+
+sudo apt-get remove wps-office steam skype skype-bin crossover;
+
+sudo apt-get purge --auto-remove nodejs;
+
+
