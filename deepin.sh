4c03b72dd4be163a5f5c554eeee3730c71e254c9
Samuel SIlva <samuelsilvawb@gmail.com>
2017-09-13 00:33:31 +0000

    Update deepin.sh

--- deepin.sh
+++ deepin.sh
@@ -21,6 +21,14 @@ sudo apt-get remove wps-office steam skype skype-bin crossover;
 
 sudo apt-get purge --auto-remove nodejs;
 
+#activate ppa
+sudo apt-get install software-properties-common;
+
+#sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - &&
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list &&
+sudo apt-get update && sudo apt-get install sublime-text;
+
 #xampp
 #eclipse
 #dbeaver
